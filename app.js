87f0b2c3194dce4ae8b176a058d7803d8eb7b72a
Ezequiel Merces <ezequielmerces36@gmail.com>
2025-12-27 11:59:34 -0300

    feat: Add conexao com o banco de dados

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const app = express();
+const db = require('./db/connection');
 
 const PORT = 3000;
+
 
 app.listen(PORT, function () {
     console.log(`O express está rodando na porta ${PORT}`)
 });
 
+//DB connection
+db
+    .authenticate()
+    .then(() => {
+        console.log("Conectou ao banco de dados com sucesso!")
+    })
+    .catch(err => {
+        console.log("Ocorreu um erro ao conectar!", err)
+    });
+
+//Routes
 app.get('/', (req, res) => {
     res.send("Está funcionando!");
 });
